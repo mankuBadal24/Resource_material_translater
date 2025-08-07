c1e1cfde76bc983ab6397864983800b74d0da78b
Mayank Badal <badalmayank23@gmail.com>
2025-08-08 00:56:19 +0530

    updates

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <title>Language Translator</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="../style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.7.1/jszip.min.js"></script>
